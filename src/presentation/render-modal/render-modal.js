09e9b4245c917110db52dbdfd4a95e7629b64fde
Momfus <momfus@outlook.com>
2022-12-21 23:06:35 -0300

    Add user finished

--- src/presentation/render-modal/render-modal.js
+++ src/presentation/render-modal/render-modal.js
@@ -20,8 +20,9 @@ export const hideModal = () => {
 /**
  * 
  * @param {HTMLDivElement} element 
+ * @param {(userLike) => Promise<void>} callback
  */
-export const renderModal = (element) => {
+export const renderModal = (element, callback) => {
 
    if( modal ) return;
 
@@ -38,7 +39,7 @@ export const renderModal = (element) => {
 
    });
 
-   form.addEventListener('submit', (event) => {
+   form.addEventListener('submit', async(event) => {
       
       event.preventDefault();  // Remove the propagation for default in the event when user select "submit"
 
@@ -62,6 +63,10 @@ export const renderModal = (element) => {
       }
 
       // console.log(userLike);
+
+      await callback(userLike);
+
+      hideModal();
 
    });
 
--- src/use-cases/save-user.js
+++ src/use-cases/save-user.js
@@ -8,10 +8,36 @@ import { User } from "../models/user";
  * 
  * @param {Like<User>} userLike 
  */
-export const saveUser = ( userLike ) => {
+export const saveUser = async ( userLike ) => {
 
    const user = new User( userLike );
    
+   // if( user.id) {
+   //    throw 'No implementada la actualización'; // return;
+   // }
 
-   // TODO: Add create user
+   const updatedUser = await createUser( user );
+
+};
+
+/**
+ * @param {Like<User>} user
+ */
+const createUser = async ( user ) => {
+
+   const url = `${ import.meta.env.VITE_BASE_URL }/users`;
+   const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+         'Content-Type': 'application/json'
+      }
+   });
+
+   const newUser = await res.json();
+   console.log({ newUser });
+
+   return newUser;
+
+
 };
